cf8f5b58a274e3619043b287d10aa7c7e48a34ef
saiyyad mohammad adil <replyadil.96@gmail.com>
2023-12-19 22:20:28 +0530

    updated code

--- js/main.js
+++ js/main.js
@@ -38,3 +38,43 @@ const observer = new IntersectionObserver(entries => {
 });
 
 observer.observe(document.querySelector('.process'));
+
+
+/**
+ * 
+ * 
+ * method to select the element by id or class
+ * 
+ */
+
+const select = (el, all = false) => {
+  el = el.trim()
+  if (all) {
+      return [...document.querySelectorAll(el)]
+  } else {
+      return document.querySelector(el)
+  }
+}
+
+/**
+ * Mobile Navigation view Function
+ */
+
+const toggleMobileView = () => {
+  let x = select('#myTopnav')
+  if (x.className === "topnav") {
+      x.className += " responsive";
+  } else {
+      x.className = "topnav";
+  }
+}
+
+var splitBar = select('.icon')
+splitBar.addEventListener('click', toggleMobileView);
+
+let navbarlinks = select('#myTopnav .scrollto', true)
+navbarlinks.forEach(
+  navbarlink => {
+      navbarlink.addEventListener('click', toggleMobileView)
+  }
+)
